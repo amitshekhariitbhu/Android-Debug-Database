aa27d5b9cf9bc489cc6bd0ca01613559a73e9aa4
AMIT SHEKHAR <amit.shekhar.iitbhu@gmail.com>
2016-11-15 23:28:58 +0530

    Add Application Class in Sample App

---
+++ app/src/main/java/com/sample/SampleApplication.java
@@ -0,0 +1,15 @@
+package com.sample;
+
+import android.app.Application;
+
+/**
+ * Created by amitshekhar on 15/11/16.
+ */
+
+public class SampleApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
