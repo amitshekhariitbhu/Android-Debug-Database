93e6a66ecc8f67f651d887a364d0a4442be479f3
AMIT SHEKHAR <amit.shekhar.iitbhu@gmail.com>
2017-02-06 14:20:06 +0530

    Modification : Standard code for finding all databases

---
+++ debug-db/src/main/java/com/amitshekhar/utils/DatabaseFileProvider.java
@@ -0,0 +1,48 @@
+/*
+ *
+ *  *    Copyright (C) 2016 Amit Shekhar
+ *  *    Copyright (C) 2011 Android Open Source Project
+ *  *
+ *  *    Licensed under the Apache License, Version 2.0 (the "License");
+ *  *    you may not use this file except in compliance with the License.
+ *  *    You may obtain a copy of the License at
+ *  *
+ *  *        http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  *    Unless required by applicable law or agreed to in writing, software
+ *  *    distributed under the License is distributed on an "AS IS" BASIS,
+ *  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *    See the License for the specific language governing permissions and
+ *  *    limitations under the License.
+ *
+ */
+
+package com.amitshekhar.utils;
+
+import android.content.Context;
+
+import java.io.File;
+import java.util.HashMap;
+
+/**
+ * Created by amitshekhar on 06/02/17.
+ */
+
+public class DatabaseFileProvider {
+
+    private DatabaseFileProvider() {
+        // This class in not publicly instantiable
+    }
+
+    public static HashMap<String, File> getDatabaseFiles(Context context) {
+        HashMap<String, File> databaseFiles = new HashMap<>();
+        try {
+            for (String databaseName : context.databaseList()) {
+                databaseFiles.put(databaseName, context.getDatabasePath(databaseName));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return databaseFiles;
+    }
+}
