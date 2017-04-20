8e51018767bb07d6fe8b2b22dfc9c39d28ed442b
Landice Fu <landicefu@kkbox.com>
2017-04-20 14:50:27 +0800

    support listing database views

--- debug-db/src/main/java/com/amitshekhar/utils/DatabaseHelper.java
+++ debug-db/src/main/java/com/amitshekhar/utils/DatabaseHelper.java
@@ -43,7 +43,7 @@ public class DatabaseHelper {
 
     public static Response getAllTableName(SQLiteDatabase database) {
         Response response = new Response();
-        Cursor c = database.rawQuery("SELECT name FROM sqlite_master WHERE type='table'", null);
+        Cursor c = database.rawQuery("SELECT name FROM sqlite_master WHERE type='table' OR type='view'", null);
         if (c.moveToFirst()) {
             while (!c.isAfterLast()) {
                 response.rows.add(c.getString(0));
