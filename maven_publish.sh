70115a502dd41c835f20399129aa7aa7685a7f5b
amitshekhariitbhu <amit.shekhar.iitbhu@gmail.com>
2024-08-18 21:55:26 +0530

    Remove Maven

--- maven_publish.sh
+++
@@ -1,5 +0,0 @@
-#upload to mavenCentral()
-#https://s01.oss.sonatype.org/#stagingRepositories
-./gradlew :debug-db:publishMavenPublicationToMavenRepository
-
-./gradlew :debug-db-encrypt:publishMavenPublicationToMavenRepository
